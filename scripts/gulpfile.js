3d52bfbc1571863d3e45a0fe0489db63fb1bd967
shuaishuai.han <shuaishuai.han@caetar.com>
2024-10-17 17:27:54 +0800

    chore: gulp构建

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -3,36 +3,23 @@ const gulp = require('gulp');
 const gulpTS = require('gulp-typescript');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
-const merge = require('merge2');
 const del = require('del');
 const tsConfig = require('./tsconfig.build.json');
 
 const ROOT_DIR = path.resolve(__dirname, '..', 'src/components');
-
-// const tsProject = gulpTS.createProject('./tsconfig.build.json', {
-//   module: 'commonjs',
-// });
 
 function compileTS({isESM}) {
   const tsStream = gulp.src(`${ROOT_DIR}/**/*.tsx`).pipe(
     gulpTS({
       ...tsConfig.compilerOptions,
       declaration: isESM,
-      emitDeclarationOnly: isESM,
       module: isESM ? 'ESnext' : 'commonjs',
     })
   );
-  // const tsStream = tsProject.src().pipe(tsProject());
 
-  if (isESM) {
-    // return tsStream.js.pipe(gulp.dest(path.resolve(__dirname, '..', 'es2/')));
-    return merge([
-      tsStream.js.pipe(gulp.dest(path.resolve(__dirname, '..', 'es'))),
-      // tsStream.dts.pipe(gulp.dest(path.resolve(__dirname, '..', 'es'))),
-    ]);
-  } else {
-    return tsStream.js.pipe(gulp.dest(path.resolve(__dirname, '..', 'lib')));
-  }
+  return tsStream.js.pipe(
+    gulp.dest(path.resolve(__dirname, '..', isESM ? 'es' : 'lib'))
+  );
 }
 
 gulp.task('buildStyles', function buildStyles() {
@@ -42,6 +29,17 @@ gulp.task('buildStyles', function buildStyles() {
     .pipe(autoprefixer())
     .pipe(gulp.dest(path.resolve(__dirname, '..', 'es')))
     .pipe(gulp.dest(path.resolve(__dirname, '..', 'lib')));
+});
+
+gulp.task('buildDts', function buildDts() {
+  const tsStream = gulp.src(`${ROOT_DIR}/**/*.tsx`).pipe(
+    gulpTS({
+      ...tsConfig.compilerOptions,
+      isolatedModules: false,
+    })
+  );
+
+  return tsStream.dts.pipe(gulp.dest(path.resolve(__dirname, '..', 'es')));
 });
 
 gulp.task('compileTSXForESM', function compileTSXForESM() {
@@ -59,7 +57,12 @@ gulp.task('clean', function clean() {
   );
 });
 
-exports.compile = gulp.series([
-  'clean',
-  gulp.parallel('compileTSXForESM', 'compileTSXForCJS', 'buildStyles'),
-]);
+gulp.task(
+  'compile',
+  gulp.series([
+    'clean',
+    gulp.parallel('compileTSXForESM', 'compileTSXForCJS'),
+    'buildDts',
+    'buildStyles',
+  ])
+);
